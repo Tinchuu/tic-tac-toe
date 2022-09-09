c549777ceb359d63eecc8ed2d78d61568fb6805a
Javalind <65702538+Javalind@users.noreply.github.com>
2022-09-10 01:58:13 +1200

    added redux states, getting them to work now

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,89 +1,52 @@
-import { Button, Grid, Paper } from "@mui/material";
-import { useState } from "react";
+import { Grid } from "@mui/material";
+import { useAppDispatch } from "../store/hooks";
+import { onMove } from "../store/slices/gameSlice";
 import PlayerDisplay from "./PlayerDisplay";
 import Square from "./Square";
 
-const [tileState, setTiles] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
-
-export interface GameState {
-    tileState: number[][];
-    gameWon: boolean;
-    player: number;
-}
-
-const initialState : GameState = {
-    tileState: [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]
-    ],
-    gameWon: false,
-    player: 1
-}
-
-export const isGameOver = (state: GameState) : boolean => {
-    return (Math.abs(state.tileState[0][0] + state.tileState[0][1] + state.tileState[0][2]) == 3) ||
-    (Math.abs(state.tileState[1][0] + state.tileState[1][1] + state.tileState[1][2]) == 3) ||
-    (Math.abs(state.tileState[2][0] + state.tileState[2][1] + state.tileState[2][2]) == 3) ||
-    (Math.abs(state.tileState[0][0] + state.tileState[1][0] + state.tileState[2][0]) == 3) ||
-    (Math.abs(state.tileState[0][1] + state.tileState[1][1] + state.tileState[2][1]) == 3) ||
-    (Math.abs(state.tileState[0][2] + state.tileState[1][2] + state.tileState[2][2]) == 3) ||
-    (Math.abs(state.tileState[0][0] + state.tileState[1][1] + state.tileState[2][2]) == 3) ||
-    (Math.abs(state.tileState[0][2] + state.tileState[1][1] + state.tileState[2][0]) == 3)
-    ;
-}
-
-function changeState(x:number, y:number, currentState:GameState) : GameState {
-    currentState.tileState[x][y] = currentState.player;
-    return currentState;
-}
-
 const GameGrid = () => {
-    let currentState = initialState;
     return(
         <div>
-            <PlayerDisplay display={currentState.player} />
+            <PlayerDisplay display={1} />
 
             <Grid container spacing={1} direction="column">
                 <Grid item spacing={1} container>
-                    <Grid onClick = {() => changeState(0,0,currentState)}item xs={2}>
-                        <Square display={currentState.tileState[0][0]}/>
+                    <Grid item xs={2}>
+                        <Square display={1}/>
                     </Grid>
                     <Grid item xs={2}>
-                        <Square display={currentState.tileState[0][1]}/>
+                        <Square display={1}/>
                     </Grid>
                     <Grid item xs={2}>
-                        <Square display={currentState.tileState[0][2]}/>
-                    </Grid>
-                </Grid>
-                <Grid item spacing={1} container>
-                    <Grid item xs={2}>
-                        <Square display={currentState.tileState[1][0]}/>
-                    </Grid>
-                    <Grid item xs={2}>
-                        <Square display={currentState.tileState[1][1]}/>
-                    </Grid>
-                    <Grid item xs={2}>
-                        <Square display={currentState.tileState[1][2]}/>
+                        <Square display={1}/>
                     </Grid>
                 </Grid>
                 <Grid item spacing={1} container>
                     <Grid item xs={2}>
-                        <Square display={currentState.tileState[2][0]}/>
+                        <Square display={1}/>
                     </Grid>
                     <Grid item xs={2}>
-                        <Square display={currentState.tileState[2][1]}/>
+                        <Square display={1}/>
                     </Grid>
                     <Grid item xs={2}>
-                        <Square display={currentState.tileState[2][2]}/>
+                        <Square display={1}/>
+                    </Grid>
+                </Grid>
+                <Grid item spacing={1} container>
+                    <Grid item xs={2}>
+                        <Square display={1}/>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <Square display={1}/>
+                    </Grid>
+                    <Grid item xs={2}>
+                        <Square display={1}/>
                     </Grid>
                 </Grid>
             </Grid>
         </div>
     )
 }
-
-
 
 
 export default GameGrid;
--- src/services/gameLogicService.ts
+++ src/services/gameLogicService.ts
@@ -1,211 +1,22 @@
-import { TileInfo } from "../store/slices/gameSlice";
+import { GameState } from "../store/slices/gameSlice";
 
-export const copyGameState = (arr: TileInfo[][]): TileInfo[][] => {
-  return arr.map((i) => i.slice());
-};
-
-export const squashRow = (row: TileInfo[]): TileInfo[] => {
-  let squashedRow = [];
-
-  for (let i = 0; i < row.length; i++) {
-    if (row[i].value !== 0) {
-      squashedRow.push(row[i]);
-    }
+export const onSquareClick = (x:number, y:number, state: GameState) : GameState => {
+  let newState = state;
+  if (state.tileState[x][y] == 0) {
+    newState.tileState[x][y] = state.player;
+    newState.player = state.player == 1 ? -1 : 1;
   }
-
-  return squashedRow;
-};
-
-export const combineLeftRowVals = (squashedRow: TileInfo[]): TileInfo[] => {
-  let newRow = [];
-
-  for (let i = 0; i < squashedRow.length; i++) {
-    if (i + 1 === squashedRow.length) {
-      newRow.push({ value: squashedRow[i].value });
-      break;
-    }
-
-    //TODO: Update score value
-    if (squashedRow[i].value === squashedRow[i + 1].value) {
-      newRow.push({ value: squashedRow[i].value + 1 });
-      i++;
-      continue;
-    }
-
-    newRow.push({ value: squashedRow[i].value });
-  }
-
-  return newRow;
-};
-
-export const combineRightRowVals = (squashedRow: TileInfo[]): TileInfo[] => {
-  let newRow = [];
-
-  for (let i = squashedRow.length - 1; i >= 0; i--) {
-    if (i - 1 < 0) {
-      newRow.unshift({ value: squashedRow[i].value });
-      break;
-    }
-
-    if (squashedRow[i].value === squashedRow[i - 1].value) {
-      newRow.unshift({ value: squashedRow[i].value + 1 });
-      i--;
-      continue;
-    }
-
-    newRow.unshift({ value: squashedRow[i].value });
-  }
-
-  return newRow;
-};
-
-export const rotateRight = (gameState: TileInfo[][]): TileInfo[][] => {
-  var newState: TileInfo[][] = [];
-  for (let i = 0; i < gameState[0].length; i++) {
-    newState.push([]);
-  }
-
-  gameState.forEach((row) => {
-    row.forEach((tile, index) => {
-      newState[index].unshift(tile);
-    });
-  });
-
   return newState;
-};
+}
 
-export const rotateLeft = (gameState: TileInfo[][]): TileInfo[][] => {
-  var newState: TileInfo[][] = [];
-  for (let i = 0; i < gameState[0].length; i++) {
-    newState.push([]);
-  }
-
-  gameState.forEach((row) => {
-    row.forEach((tile, index) => {
-      newState[row.length - index - 1].push(tile);
-    });
-  });
-
-  return newState;
-};
-
-export const generateTile = (gameState: TileInfo[][]): TileInfo[][] => {
-  let newState = copyGameState(gameState);
-
-  var emptyTiles: { x: number; y: number }[] = [];
-
-  gameState.forEach((row, rowInd) => {
-    row.forEach((tile, tileInd) => {
-      if (tile.value === 0) {
-        emptyTiles.push({ x: rowInd, y: tileInd });
-      }
-    });
-  });
-
-  let randEmpty = emptyTiles[Math.floor(Math.random() * emptyTiles.length)];
-  newState[randEmpty.x][randEmpty.y] = { value: Math.ceil(Math.random() * 2) };
-
-  return newState;
-};
-
-export const hasTileMoved = (
-  originalState: TileInfo[][],
-  newState: TileInfo[][]
-) => {
-  for (let i = 0; i < originalState.length; i++) {
-    for (let j = 0; j < originalState[i].length; j++) {
-      if (originalState[i][j].value !== newState[i][j].value) {
-        return true;
-      }
-    }
-  }
-
-  return false;
-};
-
-export const makeLeftMove = (originalState: TileInfo[][]): TileInfo[][] => {
-  return originalState.map((row) => {
-    let squashedRow = squashRow(row);
-    let newRow = combineLeftRowVals(squashedRow);
-
-    let diff = row.length - newRow.length;
-    for (let i = 0; i < diff; i++) {
-      newRow.push({ value: 0 });
-    }
-
-    return newRow;
-  });
-};
-
-export const makeRightMove = (originalState: TileInfo[][]): TileInfo[][] => {
-  return originalState.map((row) => {
-    let squashedRow = squashRow(row);
-    let newRow = combineRightRowVals(squashedRow);
-
-    let diff = row.length - newRow.length;
-    for (let i = 0; i < diff; i++) {
-      newRow.unshift({ value: 0 });
-    }
-
-    return newRow;
-  });
-};
-
-export const makeDownMove = (originalState: TileInfo[][]) : TileInfo[][] => {
-  let rotatedState = rotateRight(originalState);
-
-  let newState = rotatedState.map((row) => {
-    let squashedRow = squashRow(row);
-    let newRow = combineLeftRowVals(squashedRow);
-
-    let diff = row.length - newRow.length;
-    for (let i = 0; i < diff; i++) {
-      newRow.push({ value: 0 });
-    }
-
-    return newRow;
-  });
-
-  return rotateLeft(newState);
-};
-
-export const makeUpMove = (originalState: TileInfo[][]) : TileInfo[][] => {
-  let rotatedState = rotateLeft(originalState);
-
-  let newState = rotatedState.map((row) => {
-    let squashedRow = squashRow(row);
-    let newRow = combineRightRowVals(squashedRow);
-
-    let diff = row.length - newRow.length;
-    for (let i = 0; i < diff; i++) {
-      newRow.push({ value: 0 });
-    }
-
-    return newRow;
-  });
-
-  return rotateRight(newState);
-};
-
-export const isGameOver = (state: TileInfo[][]) : boolean => {
-    let leftState = makeLeftMove(state);
-    let rightState = makeRightMove(state);
-    let downState = makeDownMove(state);
-    let upState = makeUpMove(state);
-
-    let hasEmptyTile = false;
-
-    state.forEach((row) => {
-        row.forEach((tile) => {
-            if (tile.value === 0) {
-                hasEmptyTile = true;
-            };
-        });
-    });
-
-    return !hasEmptyTile &&
-    !hasTileMoved(state, leftState) && 
-    !hasTileMoved(state, rightState) && 
-    !hasTileMoved(state, downState) && 
-    !hasTileMoved(state, upState);
+export const isGameOver = (state: GameState) : boolean => {
+  return (Math.abs(state.tileState[0][0] + state.tileState[0][1] + state.tileState[0][2]) == 3) ||
+  (Math.abs(state.tileState[1][0] + state.tileState[1][1] + state.tileState[1][2]) == 3) ||
+  (Math.abs(state.tileState[2][0] + state.tileState[2][1] + state.tileState[2][2]) == 3) ||
+  (Math.abs(state.tileState[0][0] + state.tileState[1][0] + state.tileState[2][0]) == 3) ||
+  (Math.abs(state.tileState[0][1] + state.tileState[1][1] + state.tileState[2][1]) == 3) ||
+  (Math.abs(state.tileState[0][2] + state.tileState[1][2] + state.tileState[2][2]) == 3) ||
+  (Math.abs(state.tileState[0][0] + state.tileState[1][1] + state.tileState[2][2]) == 3) ||
+  (Math.abs(state.tileState[0][2] + state.tileState[1][1] + state.tileState[2][0]) == 3)
+  ;
 }
